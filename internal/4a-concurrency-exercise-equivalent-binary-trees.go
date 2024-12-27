8ece08385fc7c35ce1a21bfd6743e0c50f049aa7
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2024-12-27 04:13:12 -0500

    formatting

--- internal/4a-concurrency-exercise-equivalent-binary-trees.go
+++ internal/4a-concurrency-exercise-equivalent-binary-trees.go
@@ -19,18 +19,18 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-    ch1 := make(chan int)
-    ch2 := make(chan int)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-    go Walk(t1, ch1)
-    go Walk(t2, ch2)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
-    for i := 0; i < 10; i++ {
-        if <-ch1 != <-ch2 {
-            return false
-        }
-    }
-    return true
+	for i := 0; i < 10; i++ {
+		if <-ch1 != <-ch2 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
