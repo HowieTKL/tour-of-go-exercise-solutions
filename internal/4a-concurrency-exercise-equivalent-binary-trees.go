c4d28bce37b8144d4f6375e791cce775fb34fbac
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2024-12-27 04:09:56 -0500

    Update 4a-concurrency-exercise-equivalent-binary-trees.go

--- internal/4a-concurrency-exercise-equivalent-binary-trees.go
+++ internal/4a-concurrency-exercise-equivalent-binary-trees.go
@@ -22,9 +22,6 @@ func Same(t1, t2 *tree.Tree) bool {
     ch1 := make(chan int)
     ch2 := make(chan int)
 
-    defer close(ch1)
-    defer close(ch2)
-
     go Walk(t1, ch1)
     go Walk(t2, ch2)
 
@@ -33,12 +30,18 @@ func Same(t1, t2 *tree.Tree) bool {
             return false
         }
     }
-
     return true
 }
 
 func main() {
+    ch := make(chan int)
+	go Walk(tree.New(1), ch)
+    for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
+
 	fmt.Println(Same(tree.New(1), tree.New(1)))
 	fmt.Println(Same(tree.New(2), tree.New(2)))
+	fmt.Println(Same(tree.New(2), tree.New(1)))
 	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
